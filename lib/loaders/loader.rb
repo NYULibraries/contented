aad9fd329feba4500f5ac8f8ca65c5edd225bab5
amay22 <amay@nyu.edu>
2015-08-31 11:58:13 -0400

    Created loader to initialize the siteleaf.yml file

--- lib/loaders/loader.rb
+++ lib/loaders/loader.rb
@@ -1,4 +1,13 @@
 # Loads the siteleaf items
+require 'figs'
+Figs.load
 class Loader
-
+  def initialize
+    siteleaf_yml = File.open('site/.siteleaf.yml', 'w')
+    siteleaf_yml.write("---\n")
+    siteleaf_yml.write("api_key: #{ENV['api_key']}\n")
+    siteleaf_yml.write("api_secret: #{ENV['api_secret']}\n")
+    siteleaf_yml.write("site_id: #{ENV['site_id']}\n")
+  end
 end
+Loader.new
