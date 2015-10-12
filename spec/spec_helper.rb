60bb1f5a39a8bd64b6d6a770ddddf0238510cca7
amay22 <amay@nyu.edu>
2015-10-12 14:47:02 -0400

    Updated Tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 require File.expand_path('../../lib/convert/convert.rb', __FILE__)
 Dir['../../lib/convert/helpers/*.rb'].each { |file| require file }
-require 'simplecov'
-SimpleCov.start
+require 'coveralls'
+Coveralls.wear!
