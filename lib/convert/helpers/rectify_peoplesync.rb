7c4a549a405388261b42efe1bc4c0a8395c0617e
amay22 <amay@nyu.edu>
2015-10-15 19:28:42 -0400

    Added location_map.yml to correct peoplesync locations

--- lib/convert/helpers/rectify_peoplesync.rb
+++ lib/convert/helpers/rectify_peoplesync.rb
@@ -8,8 +8,8 @@ module Conversion
     # Removes People to Exclude from the peoplesync using config/people_exclude.yml
     class RectifyPeopleSync
       def remove_people_to_exclude(people)
-        fail Error, 'config/conversions/people_exclude.yml file deos not exist' unless File.exist? 'config/conversions/people_exclude.yml'
-        people_exclude = YAML.load_file('config/conversions/people_exclude.yml')['people_exclude']
+        fail Error, 'config/people_exclude.yml file deos not exist' unless File.exist? 'config/people_exclude.yml'
+        people_exclude = YAML.load_file('config/people_exclude.yml')['people_exclude']
         people.delete_if { |p| people_exclude.include? p['netid']['tx'] }
       end
     end
