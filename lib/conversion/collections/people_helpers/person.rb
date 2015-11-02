0af2dca07431160777e51297d29aad506d128dd7
amay22 <amay@nyu.edu>
2015-11-02 14:36:47 -0500

    Added a person class to match the rspec

---
+++ lib/conversion/collections/people_helpers/person.rb
@@ -0,0 +1,50 @@
+module Conversion
+  module Collections
+    module PeopleHelpers
+      # Edits the Peoplesync attributes to make them look like spreadsheet JSON
+      class Person
+        attr_accessor :netid, :employee_id, :last_name, :first_name, :primary_work_space_address, :work_phone, :email_address, :all_positions_jobs
+        PEOPLESYNC_ATTR = { netid: 'NetID', employee_id: 'Employee_ID',last_name: 'Last_Name',first_name: 'First_Name', phone: 'Work_Phone',
+                            email: 'Email_Address', address: 'Primary_Work_Space_Address', all_positions_jobs: 'All_Positions_Jobs'
+                          }
+
+        def initialize(json_data)
+          fail ArgumentError, 'None of the parameters can be nil' if json_data.nil?
+          @json_data = json_data
+        end
+
+        def netid
+          @netid ||= @json_data[PEOPLESYNC_ATTR[:netid]]
+        end
+
+        def employee_id
+          @netid ||= @json_data[PEOPLESYNC_ATTR[:employee_id]]
+        end
+
+        def last_name
+          @netid ||= @json_data[PEOPLESYNC_ATTR[:last_name]]
+        end
+
+        def first_name
+          @netid ||= @json_data[PEOPLESYNC_ATTR[:first_name]]
+        end
+
+        def primary_work_space_address
+          @netid ||= @json_data[PEOPLESYNC_ATTR[:address]]
+        end
+
+        def work_phone
+          @netid ||= @json_data[PEOPLESYNC_ATTR[:phone]]
+        end
+
+        def email_address
+          @phone ||= @json_data[PEOPLESYNC_ATTR[:email]]
+        end
+
+        def all_positions_jobs
+          @email ||= @json_data[PEOPLESYNC_ATTR[:all_positions_jobs]]
+        end
+      end
+    end
+  end
+end
