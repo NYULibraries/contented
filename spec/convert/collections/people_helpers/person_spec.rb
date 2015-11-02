481f3de74b361c55cd31daeb9d815faab0b850b9
hab278 <hab278@nyu.edu>
2015-11-02 14:55:14 -0500

    New test

--- spec/convert/collections/people_helpers/person_spec.rb
+++ spec/convert/collections/people_helpers/person_spec.rb
@@ -1,12 +1,16 @@
 require File.expand_path('../../../../spec_helper.rb', __FILE__)
 
+def attributes
+  %w[netid employee_id last_name first_name
+  primary_work_space_address work_phone
+  email_address all_positions_jobs]
+end
+
 describe 'Person' do
   let(:json_data) { '' }
   let(:person) { Conversion::Collections::PeopleHelpers::Person.new(json_data) }
   context "when no JSON formatted data is provided" do
-    %w[netid employee_id last_name first_name
-    primary_work_space_address work_phone
-    email_address all_positions_jobs].each do |attribute|
+    attributes.each do |attribute|
       it "should not have #{attribute}" do
         person.send( attribute.to_sym ).should be_nil
       end
@@ -39,12 +43,14 @@ describe 'Person' do
          ]
       }.to_json
     }
-    %w[netid employee_id last_name first_name
-    primary_work_space_address work_phone
-    email_address all_positions_jobs].each do |attribute|
+    attributes.each do |attribute|
       it "should have #{attribute}" do
         person.send( attribute.to_sym ).should_not be_nil
       end
+    end
+
+    it "should have the #{attributes.size} instance variables" do
+      person.instance_variables.size.should eql attributes.size
     end
   end
 end
