70d4cb82d035d8993f0c5d9661591e52de55b848
amay22 <amay@nyu.edu>
2015-11-03 11:56:15 -0500

    Modified Person class to remove the dependancy from PeopleSync_Attr

--- lib/conversion/collections/people_helpers/person.rb
+++ lib/conversion/collections/people_helpers/person.rb
@@ -1,16 +1,23 @@
+require 'json'
+
 module Conversion
   module Collections
     module PeopleHelpers
       # Edits the Peoplesync attributes to make them look like spreadsheet JSON
       class Person
         attr_accessor :netid, :employee_id, :last_name, :first_name, :primary_work_space_address, :work_phone, :email_address, :all_positions_jobs
-        PEOPLESYNC_ATTR = { netid: 'NetID', employee_id: 'Employee_ID',last_name: 'Last_Name',first_name: 'First_Name', work_phone: 'Work_Phone',
-                            email_address: 'Email_Address', primary_work_space_address: 'Primary_Work_Space_Address', all_positions_jobs: 'All_Positions_Jobs'
-                          }
 
         def initialize(json_data)
           fail ArgumentError, 'None of the parameters can be nil' if json_data.nil?
-          PEOPLESYNC_ATTR.each_pair { |variable, val| instance_variable_set("@#{variable}", json_data[val]) }
+          JSON.parse(json_data)['Report_Entry'][0].each_pair { |k,v| instance_variable_set("@#{k.downcase}", v) if respond_to?(k.downcase) } if valid_json? json_data
+        end
+
+        def valid_json?(json_data)
+            JSON.parse(json_data)
+            return true
+          rescue Exception => e
+            # puts 'Incorrect JSON FORMAT'
+            return false
         end
       end
     end
