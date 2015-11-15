c5d1667aa88e0a3503c31468bccdd46b36a65658
amay22 <amay@nyu.edu>
2015-11-15 15:02:34 -0500

    Added Expanded Person class to combine sheet and person data

---
+++ lib/conversion/collections/people_helpers/expanded_person.rb
@@ -0,0 +1,19 @@
+require 'forwardable'
+require_relative 'person'
+require_relative 'google_spreadsheet_person'
+
+module Conversion
+  module Collections
+    module PeopleHelpers
+      # Combines Person class attributes with google_spreadsheet_person class attributes
+      class ExpandedPerson < Person
+        extend Forwardable
+        def_delegators :@GoogleSpreadsheetPerson, :about, :address, :buttons, :departments, :email, :expertise, :guides, :image, :jobtitle, :keywords, :location, :netid, :phone, :space, :status, :subtitle, :title, :twitter
+        def initialize(json_data, json_data_expand={})
+          super(json_data)
+          @GoogleSpreadsheetPerson = GoogleSpreadsheetPerson.new(json_data_expand.to_s)
+        end
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require File.expand_path('../../lib/conversion/convert.rb', __FILE__)
 Dir['../../lib/conversion/helpers/*.rb'].each { |file| require file }
 require File.expand_path('../../lib/conversion/collections/people_helpers/person.rb', __FILE__)
 require File.expand_path('../../lib/conversion/collections/people_helpers/google_spreadsheet_person.rb', __FILE__)
+require File.expand_path('../../lib/conversion/collections/people_helpers/expanded_person.rb', __FILE__)
 require 'coveralls'
 Coveralls.wear!
 
