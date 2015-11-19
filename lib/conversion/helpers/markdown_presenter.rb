b6925eaa476f29d17bd11710073d8db28d6182db
amay22 <amay@nyu.edu>
2015-11-19 11:56:20 -0500

    Added a markdown presenter for the Person Class

---
+++ lib/conversion/helpers/markdown_presenter.rb
@@ -0,0 +1,105 @@
+class MarkdownPresenter
+  attr_reader :person
+  def initialize(person)
+    @person = person
+  end
+
+  def render
+    render = ""
+    (self.private_methods - Object.private_methods - Module.methods).each do |method_sym|
+      render = "#{render}#{self.send(method_sym)}\n"
+    end
+    render
+  end
+
+private
+
+  def yaml_start
+    "---\n"
+  end
+
+  def subtitle
+    "subtitle: '#{person.subtitle}'"
+  end
+
+  def job_title
+    "job_title: '#{person.title}'"
+  end
+
+  def location
+    "location: '#{person.location}'"
+  end
+
+  def space
+    "space: '#{person.space}'"
+  end
+
+  def departments
+    "departments: '#{person.departments}'"
+  end
+
+  def status
+    "status: '#{person.status}'"
+  end
+
+  def expertise
+    "expertise: '#{person.expertise}'"
+  end
+
+  def email
+    "email: '#{person.email}'"
+  end
+
+  def phone
+    "phone: '#{person.phone}'"
+  end
+
+  def twitter
+    "twitter: '#{person.twitter}'"
+  end
+
+  def image
+    "image: '#{person.image}'"
+  end
+
+  def buttons
+    "buttons: '#{person.button}'"
+  end
+
+  def guides
+    "guides: '#{person.guides}'"
+  end
+
+  def publications
+    "publications: '#{person.publications}'"
+  end
+
+  def keywords
+    "keywords: '#{person.keywords}'"
+  end
+
+  def title
+    "title: '#{person.title}'"
+  end
+
+  def yaml_end
+    "\n---\n"
+  end
+
+  def about_block
+    "# About #{person.name}"
+  end
+end
+
+class Person
+  def initialize
+  end
+
+  def method_missing(m, *args, &block)
+    m
+  end
+end
+
+person = Person.new
+markdownPresenter = MarkdownPresenter.new(person)
+puts markdownPresenter.render
