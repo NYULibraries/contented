5d02e8ed5dbafe9924e6435e39eabc87cf110ddd
amay22 <amay@nyu.edu>
2015-11-19 12:17:06 -0500

    Added attribute Publications to the delegator for expanded person class

--- lib/conversion/collections/people_helpers/expanded_person.rb
+++ lib/conversion/collections/people_helpers/expanded_person.rb
@@ -8,7 +8,7 @@ module Conversion
       # Combines Person class attributes with google_spreadsheet_person class attributes
       class ExpandedPerson < Person
         extend Forwardable
-        def_delegators :@GoogleSpreadsheetPerson, :about, :address, :buttons, :departments, :email, :expertise, :guides, :image, :jobtitle, :keywords, :location, :netid, :phone, :space, :status, :subtitle, :title, :twitter
+        def_delegators :@GoogleSpreadsheetPerson, :about, :address, :buttons, :departments, :email, :expertise, :guides, :image, :jobtitle, :keywords, :location, :netid, :phone, :space, :status, :subtitle, :title, :twitter, :publications
         def initialize(json_data, json_data_expand={})
           super(json_data)
           @GoogleSpreadsheetPerson = GoogleSpreadsheetPerson.new(json_data_expand.to_s)
