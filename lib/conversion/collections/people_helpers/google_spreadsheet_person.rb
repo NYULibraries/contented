bbcb75c4084a17f98c98740828a8ee37c80a1c9b
amay22 <amay@nyu.edu>
2015-11-19 12:15:36 -0500

    Added attribute Publications to the google person sheet class

--- lib/conversion/collections/people_helpers/google_spreadsheet_person.rb
+++ lib/conversion/collections/people_helpers/google_spreadsheet_person.rb
@@ -8,7 +8,7 @@ module Conversion
       # Edits the Google Sheet People workbook for markdown conversion
       class GoogleSpreadsheetPerson < Person
         include GoogleSpreadsheetHelpers
-        attr_accessor :about, :address, :buttons, :departments, :email, :expertise, :guides, :image, :jobtitle, :keywords, :location, :netid, :phone, :space, :status, :subtitle, :title, :twitter
+        attr_accessor :about, :address, :buttons, :departments, :email, :expertise, :guides, :image, :jobtitle, :keywords, :location, :netid, :phone, :space, :status, :subtitle, :title, :twitter, :publications
 
         def initialize(json_data)
           useful_spreadsheet_hash(JSON.parse(json_data)).each_pair { |var, val| send("#{var}=", val) if respond_to?(var) }
