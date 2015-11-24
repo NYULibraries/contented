312c688919a8e276323e1a7e42d51e80fb0c2de8
amay22 <amay@nyu.edu>
2015-11-24 13:20:16 -0500

    Added spec test for person exhibitor when only json data from peoplesync is provided

--- spec/convert/collections/people_helpers/person_exhibitor_spec.rb
+++ spec/convert/collections/people_helpers/person_exhibitor_spec.rb
@@ -129,6 +129,43 @@ describe 'PersonExhibitor' do
     end
   end
 
+  context 'when only json_data_expand is provided i.e. spreadsheet data' do
+    let(:json_data) {
+      {
+         NetID: "xx123",
+         Employee_ID: "N00000001",
+         Last_Name: "Lastname",
+         First_Name: "Firstname",
+         Primary_Work_Space_Address: "10 Number Place",
+         Work_Phone: "+1 (555) 5555555",
+         Email_Address: "no-reply@nyu.edu",
+         All_Positions_Jobs:[
+            {
+               Job_Profile: "000000 - Some Job Profile",
+               Is_Primary_Job: "1",
+               Job_Family_Group: "NYU - Something",
+               Supervisory_Org_Name: "Some Group",
+               Business_Title: "Super Fancy Title",
+               Position_Work_Space: "Earth > America > New York > New York",
+               Division_Name: "Division of Tests"
+            }
+         ]
+      }.to_json
+    }
+    let(:json_data_expand) { '{}' }
+    subject(:person_exhibitor) { Conversion::Collections::PeopleHelpers::PersonExhibitor.new(json_data, json_data_expand) }
+
+    person_exhibitor_attributes.each do |attribute|
+      it "should have #{attribute}" do
+        expect(person_exhibitor).to respond_to attribute
+      end
+    end
+
+    it 'should be a person' do
+      expect(person_exhibitor).to be_a_kind_of(Conversion::Collections::PeopleHelpers::ExpandedPerson)
+    end
+  end
+
   context 'when proper JSON formatted data is provided' do
     let(:json_data) {
       {
