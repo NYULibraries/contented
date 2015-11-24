8202f487ac8596d4956af34abc698a8a119f684c
amay22 <amay@nyu.edu>
2015-11-24 12:42:55 -0500

    Added person_exhibitor to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ Dir['../../lib/conversion/helpers/*.rb'].each { |file| require file }
 require File.expand_path('../../lib/conversion/collections/people_helpers/person.rb', __FILE__)
 require File.expand_path('../../lib/conversion/collections/people_helpers/google_spreadsheet_person.rb', __FILE__)
 require File.expand_path('../../lib/conversion/collections/people_helpers/expanded_person.rb', __FILE__)
+require File.expand_path('../../lib/conversion/collections/people_helpers/person_exhibitor.rb', __FILE__)
 require 'coveralls'
 Coveralls.wear!
 
