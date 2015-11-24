1a84e92b16f458cc448103bd6422f38853004f3c
amay22 <amay@nyu.edu>
2015-11-24 13:18:28 -0500

    Added spec test for person exhibitor when only spreadsheet json is provided

--- spec/convert/collections/people_helpers/person_exhibitor_spec.rb
+++ spec/convert/collections/people_helpers/person_exhibitor_spec.rb
@@ -30,7 +30,106 @@ describe 'PersonExhibitor' do
     end
   end
 
-  context "when proper JSON formatted data is provided" do
+  context 'when only json_data_expand is provided i.e. spreadsheet data' do
+    let(:json_data) { '{}'}
+    let(:json_data_expand) {
+      {
+        id: {
+          :$t => "https://spreadsheets.google.com/feeds/list/V8nNaf-000k1dESZGCwBvkuxoCe000k1dESZGCwBvkuxoCe/0/public/values/bpgoi"
+        },
+        updated: {
+          :$t => "2015-10-28T18:47:00.245Z"
+        },
+        category: [{
+            scheme: "http://schemas.google.com/spreadsheets/2006",
+            term: "http://schemas.google.com/spreadsheets/2006#list"
+        }],
+        title: {
+            type: "text",
+            :$t => "xx99"
+        },
+        content: {
+            type: "text",
+            :$t => ""
+        },
+        link: [{
+            rel: "self",
+            type: "application/atom+xml",
+            href: "https://spreadsheets.google.com/feeds/list/V8nNaf-000k1dESZGCwBvkuxoCe000k1dESZGCwBvkuxoCe/0/public/values/bpgoi"
+        }],
+        "gsx$netid" => {
+          :$t => "xx99"
+        },
+        "gsx$subtitle" => {
+          :$t => "subtitle"
+        },
+        "gsx$expertise" => {
+          :$t => "Everything"
+        },
+        "gsx$twitter" => {
+          :$t => "tweeter"
+        },
+        "gsx$image" => {
+          :$t => "image"
+        },
+        "gsx$buttons" => {
+          :$t => "buttons"
+        },
+        "gsx$guides" => {
+          :$t => "title: Title ;\nlibguide_id: number"
+        },
+        "gsx$publications" => {
+          :$t => "Publications"
+        },
+        "gsx$keywords" => {
+          :$t => "Key Word"
+        },
+        "gsx$about" => {
+          :$t => "This is test data about"
+        },
+        "gsx$title" => {
+          :$t => "Title"
+        },
+        "gsx$phone" => {
+          :$t => "(555) 555-5555"
+        },
+        "gsx$email" => {
+          :$t => "not@nemail"
+        },
+        "gsx$address" => {
+          :$t => "123 Sesame St"
+        },
+        "gsx$departments" => {
+          :$t => "Astroland"
+        },
+        "gsx$location" => {
+          :$t => "Coney Island"
+        },
+        "gsx$space" => {
+          :$t => "Some Floor"
+        },
+        "gsx$status" => {
+          :$t => "Status"
+        },
+        "gsx$jobtitle" => {
+          :$t => "Jobtitle"
+        }
+      }.to_json
+    }
+    subject(:person_exhibitor) { Conversion::Collections::PeopleHelpers::PersonExhibitor.new(json_data, json_data_expand) }
+
+    person_exhibitor_attributes.each do |attribute|
+      it "should have #{attribute}" do
+        expect(person_exhibitor).to respond_to attribute
+      end
+    end
+
+    it 'should be a person' do
+      expect(person_exhibitor).to be_a_kind_of(Conversion::Collections::PeopleHelpers::ExpandedPerson)
+    end
+  end
+
+  context 'when proper JSON formatted data is provided' do
     let(:json_data) {
       {
          NetID: "xx123",
