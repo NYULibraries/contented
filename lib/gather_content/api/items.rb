c99c524a7f6ad4e041581a9d99301f5562968850
B Alter <barnaby.alter@gmail.com>
2015-11-30 16:07:07 -0500

    Created an item_class method which defines what an Items collection considers an item in the each method

--- lib/gather_content/api/items.rb
+++ lib/gather_content/api/items.rb
@@ -2,22 +2,28 @@ module GatherContent
   module Api
     class Items < Base
       include Enumerable
-      attr_accessor :project_id, :items
+      attr_accessor :project_id
+
+      extend Forwardable
+      def_delegators :to_a, :each
 
       def initialize(project_id)
         raise ArgumentError, "Project_id is required!" if project_id.nil?
         @project_id = project_id
       end
 
-      extend Forwardable
-      def_delegator :to_a, :each
-
       def to_a(items = [])
-        JSON.parse(get.body)["data"].each do |item|
-          new_item = GatherContent::Api::Item.new(item["id"])
+        JSON.parse(get.body)['data'].each do |item|
+          new_item = item_class.new(item['id'])
           items << new_item
         end
         return items
+      end
+
+    protected
+
+      def item_class
+        @item_class ||= Kernel.const_get("GatherContent::Api::Item")
       end
 
     private
