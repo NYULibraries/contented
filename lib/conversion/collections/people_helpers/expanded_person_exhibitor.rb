6525ec0b4ca43db8e6c84e79471064167a41705d
amay22 <amay@nyu.edu>
2015-12-01 15:17:59 -0500

    Fixed some errors and added jobtitle to expanded_person_exhibitor

--- lib/conversion/collections/people_helpers/expanded_person_exhibitor.rb
+++ lib/conversion/collections/people_helpers/expanded_person_exhibitor.rb
@@ -13,6 +13,8 @@ module Conversion
           email
           phone
           departments
+          location_space
+          jobtitle
         end
 
         def to_markdown
@@ -28,23 +30,38 @@ module Conversion
         end
 
         def correct_job_position
-          expanded_person.all_position_jobs.each do |job|
+          expanded_person.all_positions_jobs.each do |job|
             @correct_job_position ||= job if job['Is_Primary_Job'] == '1'
           end
+          @correct_job_position
         end
 
         def email
-          expanded_person.instance_variable_get('@GoogleSpreadsheetPerson').email = expanded_person.email_address if expanded_person.email.empty?
+          expanded_person.instance_variable_get('@GoogleSpreadsheetPerson').email = expanded_person.email_address if expanded_person.email.to_s.empty?
         end
 
         def phone
-          expanded_person.instance_variable_get('@GoogleSpreadsheetPerson').phone = phone_formatter(expanded_person.work_phone) if expanded_person.phone.empty?
+          expanded_person.instance_variable_get('@GoogleSpreadsheetPerson').phone = phone_formatter(expanded_person.work_phone) if expanded_person.phone.to_s.empty?
         end
 
         def departments
-          if expanded_person.departments.empty? && correct_job_position && correct_job_position['Supervisory_Org_Name']
+          if expanded_person.departments.to_s.empty? && correct_job_position && correct_job_position['Supervisory_Org_Name']
             # Must return anything before the '(' if it exists
             expanded_person.instance_variable_get('@GoogleSpreadsheetPerson').departments ||= correct_job_position['Supervisory_Org_Name'].split('(')[0].strip
+          end
+        end
+
+        def location_space
+          if correct_job_position && correct_job_position['Position_Work_Space']
+            city, location, space = correct_job_position['Position_Work_Space'].split('>')
+            expanded_person.instance_variable_get('@GoogleSpreadsheetPerson').location = location if expanded_person.location.to_s.empty?
+            expanded_person.instance_variable_get('@GoogleSpreadsheetPerson').space = space if expanded_person.space.to_s.empty?
+          end
+        end
+
+        def jobtitle
+          if expanded_person.jobtitle.to_s.empty? && correct_job_position && correct_job_position['Business_Title']
+            expanded_person.instance_variable_get('@GoogleSpreadsheetPerson').jobtitle = correct_job_position['Business_Title']
           end
         end
       end
