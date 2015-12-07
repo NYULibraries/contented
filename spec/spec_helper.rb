9a228423ac2d8bfe7ca66ad884912d46133a4863
amay22 <amay@nyu.edu>
2015-12-07 13:59:02 -0500

    Added Google Sheet Person class and its helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
 require File.expand_path('../../lib/conversions/collections/people_helpers/person.rb', __FILE__)
+require File.expand_path('../../lib/conversions/collections/people_helpers/google_spreadsheet_person.rb', __FILE__)
 require 'coveralls'
 Coveralls.wear!
