b94c777b8b2f572e47e5a48a5258be4dd7b5be28
amay22 <amay@nyu.edu>
2015-12-08 14:24:17 -0500

    Updated person and expanded_person spec for empty array for all_positions_jobs and removed to_markdown spec

--- spec/conversions/collections/people_helpers/person_spec.rb
+++ spec/conversions/collections/people_helpers/person_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../../../spec_helper.rb', __FILE__)
 def attributes
   %w[netid employee_id last_name first_name
   primary_work_space_address work_phone
-  email_address all_positions_jobs]
+  email_address all_positions_jobs title]
 end
 
 describe 'Person' do
@@ -11,13 +11,10 @@ describe 'Person' do
   subject(:person) { Conversions::Collections::PeopleHelpers::Person.new(json_data) }
   context "when no JSON formatted data is provided" do
     attributes.each do |attribute|
+      next if attribute == 'all_positions_jobs'
       it "should not have #{attribute}" do
         expect(person.send( attribute.to_sym )).to be_nil
       end
-    end
-
-    it "should be able to convert to markdown" do
-      expect(person).to respond_to :to_markdown
     end
   end
   context "when proper JSON formatted data is provided" do
@@ -51,10 +48,6 @@ describe 'Person' do
 
     it "should have the #{attributes.size} instance variables" do
       expect(person.instance_variables.size).to eql(attributes.size)
-    end
-
-    it "should be able to convert to markdown" do
-      expect(person).to respond_to :to_markdown
     end
   end
 end
