01d720e10866e4873228983d0d3a3a4085d1a038
amay22 <amay@nyu.edu>
2015-12-08 14:21:05 -0500

    Varibales assign to nil if empty and removed to_markdown

--- lib/conversions/collections/people_helpers/google_spreadsheet_person.rb
+++ lib/conversions/collections/people_helpers/google_spreadsheet_person.rb
@@ -11,10 +11,7 @@ module Conversions
         attr_accessor :about, :address, :buttons, :departments, :email, :expertise, :guides, :image, :jobtitle, :keywords, :location, :netid, :phone, :space, :status, :subtitle, :title, :twitter, :publications
 
         def initialize(json_data)
-          useful_spreadsheet_hash(JSON.parse(json_data)).each_pair { |var, val| send("#{var}=", val) if respond_to?(var) }
-        end
-
-        def to_markdown
+          useful_spreadsheet_hash(JSON.parse(json_data)).each_pair { |var, val| send("#{var}=", val) if respond_to?(var) && !val.empty? }
         end
       end
     end
