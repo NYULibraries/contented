d6ab9e74eabbeaf75556575777ead916e8b34101
amay22 <amay@nyu.edu>
2015-12-09 13:38:51 -0500

    Added spec for expanded person exhibitor and updated spec_helper accordingly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../../lib/conversions/collections/people_helpers/person.rb', __FILE__)
 require File.expand_path('../../lib/conversions/collections/people_helpers/google_spreadsheet_person.rb', __FILE__)
 require File.expand_path('../../lib/conversions/collections/people_helpers/expanded_person.rb', __FILE__)
+require File.expand_path('../../lib/conversions/collections/people_helpers/expanded_person_exhibitor.rb', __FILE__)
 require 'coveralls'
 Coveralls.wear!
