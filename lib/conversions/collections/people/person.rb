24ca442f961a2b6f220e80d2019170aee25d8e73
amay22 <amay@nyu.edu>
2015-12-11 15:27:39 -0500

    Removed unecessary attr_accessors from primitive person objects and updated

--- lib/conversions/collections/people/person.rb
+++ lib/conversions/collections/people/person.rb
@@ -5,12 +5,12 @@ module Conversions
     module People
       # Edits the Peoplesync attributes to make them look like spreadsheet JSON
       class Person
-        attr_accessor :netid, :employee_id, :last_name, :first_name, :primary_work_space_address, :work_phone, :email_address, :all_positions_jobs, :title
+        attr_accessor :netid, :last_name, :first_name, :work_phone, :email_address, :all_positions_jobs, :backup_title
 
         def initialize(json_data)
           JSON.parse(json_data).each_pair { |var, val| self.send("#{var.downcase}=", val) if respond_to?(var.downcase) && !val.empty? }
           @all_positions_jobs = [] unless @all_positions_jobs
-          @title = first_name + last_name if first_name && last_name
+          @backup_title = "#{first_name} #{last_name}" if first_name && last_name
         end
       end
     end
