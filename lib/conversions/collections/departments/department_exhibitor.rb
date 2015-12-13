560dd977428d213dad888be2f452b8da93467f33
amay22 <amay@nyu.edu>
2015-12-13 14:57:49 -0500

    Added an exhibitor for department class

---
+++ lib/conversions/collections/departments/department_exhibitor.rb
@@ -0,0 +1,41 @@
+require_relative 'department'
+require_relative 'presenter/markdown_presenter'
+require_relative '../helpers/markdown_field_helpers'
+
+module Conversions
+  module Collections
+    module Departments
+      # Parses the person data into the required format.
+      class DepartmentExhibitor
+        extend Forwardable
+        include Conversions::Collections::Helpers::MarkdownFieldHelpers
+        def_delegators :@department, :title, :subtitle, :location, :space, :email, :phone, :twitter, :facebook, :blog, :libcal_id, :libanswers_id, :links, :classes, :image, :buttons, :keywords, :whatwedo
+        attr_reader :department
+
+        def initialize(department)
+          @department = department
+        end
+
+        def to_markdown
+          Presenter::MarkdownPresenter.new(self).run
+        end
+
+        def blog
+          to_yaml_object(department.blog)
+        end
+
+        def classes
+          to_yaml_object(department.classes)
+        end
+
+        def links
+          to_yaml_object(department.links)
+        end
+
+        def keywords
+          to_yaml_list(department.keywords)
+        end
+      end
+    end
+  end
+end
