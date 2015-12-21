cd660a286893dad0879b04269a5822cfb3ab7147
amay22 <amay@nyu.edu>
2015-12-21 00:21:29 -0500

    Renamed the def_delegators and add all the varibales useful or not

--- lib/conversions/collections/people/expanded_person.rb
+++ lib/conversions/collections/people/expanded_person.rb
@@ -8,12 +8,14 @@ module Conversions
       # Combines Person class attributes with google_spreadsheet_person class attributes
       class ExpandedPerson
         extend Forwardable
-        def_delegators :@Person, :work_phone, :email_address, :all_positions_jobs, :backup_title
-        def_delegators :@GoogleSpreadsheetPerson, :address, :buttons, :departments, :email, :expertise, :guides, :image, :jobtitle, :keywords, :location, :phone, :space, :status, :subtitle, :title, :twitter, :publications
+        def_delegators :@person, :netid, :last_name, :first_name, :work_phone, :email_address, :all_positions_jobs
+        def_delegators :@google_sheet_person, :address, :buttons, :departments, :email, :expertise,
+                       :guides, :image, :jobtitle, :keywords, :location, :netid, :phone, :space,
+                       :status, :subtitle, :title, :twitter, :publications
 
         def initialize(person, google_person)
-          @Person = person
-          @GoogleSpreadsheetPerson = google_person
+          @person = person ? person : Person.new('{}')
+          @google_sheet_person = google_person ? google_person : GoogleSpreadsheetPerson.new('{}')
         end
       end
     end
