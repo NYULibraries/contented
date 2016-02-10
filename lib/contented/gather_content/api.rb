f1404bb312f4b2ac0f596b29e831da87ad30ba65
hab278 <hab278@nyu.edu>
2016-02-10 13:36:16 -0500

    Move api out

--- lib/contented/gather_content/api.rb
+++
@@ -1,9 +0,0 @@
-module Contented
-  module GatherContent
-    module Api
-      autoload :Base, 'contented/gather_content/api/base'
-      autoload :Items, 'contented/gather_content/api/items'
-      autoload :Item, 'contented/gather_content/api/item'
-    end
-  end
-end
