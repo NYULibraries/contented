0389626396f779e340deff52bc896f667680ad5e
hab278 <hab278@nyu.edu>
2016-02-10 13:36:36 -0500

    Remove all use of gather_content

--- lib/contented/gather_content.rb
+++
@@ -1,8 +0,0 @@
-module Contented
-  module GatherContent
-    autoload :Departments, 'contented/gather_content/departments'
-    autoload :Department, 'contented/gather_content/department'
-    autoload :Api, 'contented/gather_content/api'
-    autoload :Decorators, 'contented/gather_content/decorators'
-  end
-end
