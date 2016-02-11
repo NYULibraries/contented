f7e22a1a4ec75fd26b4239ebde3be641a758e980
hab278 <hab278@nyu.edu>
2016-02-11 16:20:14 -0500

    Load up all required gems and helpers in contented

--- lib/contented.rb
+++ lib/contented.rb
@@ -1,11 +1,13 @@
 require 'json'
 require 'faraday'
 require 'gather_content'
+require 'RestClient'
 
 module Contented
   require 'contented/version'
   autoload :Decorators, 'contented/decorators'
   autoload :Conversions, 'contented/conversions'
+  autoload :Helpers, 'contented/helpers'
   autoload :Department, 'contented/department'
   autoload :Departments, 'contented/departments'
 end
