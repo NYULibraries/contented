7af4c8d78e8c12bb3e39d3490f1f3827b5dafb7c
hab278 <hab278@nyu.edu>
2016-03-01 12:24:02 -0500

    Include peoplesync helpers into namespace

--- lib/contented/helpers.rb
+++ lib/contented/helpers.rb
@@ -1,5 +1,6 @@
 module Contented
   module Helpers
     autoload :TitleHelpers, 'contented/helpers/title_helpers'
+    autoload :PeopleSyncHelpers, 'contented/helpers/people_sync_helpers'
   end
 end
