d6b5600eb6309b0e0c6623ffc4770a0da5960400
hab278 <hab278@nyu.edu>
2016-03-02 14:53:55 -0500

    Handle peoplesync environment variables better

--- lib/contented/helpers/people_sync_helpers.rb
+++ lib/contented/helpers/people_sync_helpers.rb
@@ -8,10 +8,31 @@ module Contented
 
       private
 
+      def people_sync_error
+        raise Exception.new("Please ensure all PeopleSync environment variables are set
+        ('people_sync_host', 'people_sync_url', 'people_sync_user', 'people_sync_pass').")
+      end
+
+      def people_sync_host
+        ENV['people_sync_host'] or people_sync_error
+      end
+
+      def people_sync_url
+        ENV['people_sync_url'] or people_sync_error
+      end
+
+      def people_sync_user
+        ENV['people_sync_user'] or people_sync_error
+      end
+
+      def people_sync_pass
+        ENV['people_sync_pass'] or people_sync_error
+      end
+
       def people_sync_connection
-        @people_sync_connection ||= Faraday.new(url: ENV['people_sync_host']) do |faraday|
+        @people_sync_connection ||= Faraday.new(url: people_sync_host) do |faraday|
           faraday.request  :url_encoded
-          faraday.request  :basic_auth, ENV['people_sync_user'], ENV['people_sync_pass']
+          faraday.request  :basic_auth, people_sync_user, people_sync_pass
           faraday.response :logger
           faraday.adapter  Faraday.default_adapter
         end
@@ -19,7 +40,7 @@ module Contented
 
       def people_sync_response
         response = people_sync_connection.get do |request|
-          request.url ENV['people_sync_url']
+          request.url people_sync_url
           request.params = {format: "json"}
           request.headers['Accept'] = "application/json"
         end
