8fdba025dd90f7cf1cedb686499883c8e838c1ea
hab278 <hab278@nyu.edu>
2016-03-09 11:05:32 -0500

    Allow for accessing nyuad/sh spreadsheet

--- lib/contented/helpers/google_spreadsheet_helpers.rb
+++ lib/contented/helpers/google_spreadsheet_helpers.rb
@@ -5,8 +5,16 @@ module Contented
         "http://spreadsheets.google.com/feeds/list/1dulIx-iDMH4R1RwHfZs_HPGuvslQTCNcGNAkxim0v5k/#{sheet_num}/public/values?alt=json"
       end
 
+      def nyu_ad_sh_uri(sheet_num)
+        "https://spreadsheets.google.com/feeds/list/1pSII2f9djWuIzrdFwGrwqV9GTNDpAX1g8ES_le2_6_k/#{sheet_num}/public/values?alt=json"
+      end
+
       def google_sheet_json(sheet_num)
         JSON.parse(open(uri(sheet_num)).read)['feed']['entry']
+      end
+
+      def google_sheet_json_nyu_ad_sh(sheet_num)
+        JSON.parse(open(nyu_ad_sh_uri(sheet_num)).read)['feed']['entry']
       end
 
       def find_in_json(people_sheet=[], netid)
