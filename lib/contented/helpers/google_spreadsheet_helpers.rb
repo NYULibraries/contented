4cbc4fc7660f4fa65ff206a67e90f33e76b2c2c9
hab278 <hab278@nyu.edu>
2016-03-14 15:45:25 -0400

    Update googlespreadsheet helpers to use environment vars

--- lib/contented/helpers/google_spreadsheet_helpers.rb
+++ lib/contented/helpers/google_spreadsheet_helpers.rb
@@ -1,20 +1,18 @@
 module Contented
   module Helpers
     module GoogleSpreadsheetHelpers
-      def uri(sheet_num)
-        "http://spreadsheets.google.com/feeds/list/1dulIx-iDMH4R1RwHfZs_HPGuvslQTCNcGNAkxim0v5k/#{sheet_num}/public/values?alt=json"
+
+      def google_sheet_error(environment_var_name)
+        raise Exception.new("Please ensure all Google Spreadsheet environment variables are set
+        (#{environment_var_name}).")
       end
 
-      def nyu_ad_sh_uri(sheet_num)
-        "https://spreadsheets.google.com/feeds/list/1pSII2f9djWuIzrdFwGrwqV9GTNDpAX1g8ES_le2_6_k/#{sheet_num}/public/values?alt=json"
+      def sheet_uri(environment_var_name)
+        ENV[environment_var_name] or google_sheet_error
       end
 
-      def google_sheet_json(sheet_num)
-        JSON.parse(open(uri(sheet_num)).read)['feed']['entry']
-      end
-
-      def google_sheet_json_nyu_ad_sh(sheet_num)
-        JSON.parse(open(nyu_ad_sh_uri(sheet_num)).read)['feed']['entry']
+      def google_sheet_json(environment_var_name = 'google_sheet_uri')
+        JSON.parse(open(sheet_uri(environment_var_name)).read)['feed']['entry']
       end
 
       def find_in_json(people_sheet=[], netid)
