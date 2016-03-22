d8a737406b5f64b27660d21125a7f9faea56844e
hab278 <hab278@nyu.edu>
2016-03-22 18:17:13 -0400

    Add liaison_relationship metafield

--- lib/contented/conversions/collections/people/google_spreadsheet_person.rb
+++ lib/contented/conversions/collections/people/google_spreadsheet_person.rb
@@ -8,6 +8,7 @@ module Contented
           attr_accessor :address, :buttons, :departments, :email, :expertise, :guides,
                         :image, :jobtitle, :keywords, :library, :netid, :phone, :space,
                         :status, :subtitle, :title, :twitter, :publications, :blog, :about
+                        :liaison_relationship
 
           def initialize(json_data)
             useful_spreadsheet_hash(JSON.parse(json_data)).each_pair { |var, val| send("#{var}=", val) if respond_to?(var) && !val.empty? }
