ae5e183119471f8dd2e06370b44de174ab876662
hab278 <hab278@nyu.edu>
2016-03-24 14:24:35 -0400

    Forgot an important comma

--- lib/contented/conversions/collections/people/google_spreadsheet_person.rb
+++ lib/contented/conversions/collections/people/google_spreadsheet_person.rb
@@ -7,7 +7,7 @@ module Contented
           include Conversions::Collections::Helpers::GoogleSpreadsheetHelpers
           attr_accessor :address, :buttons, :departments, :email, :expertise, :guides,
                         :image, :jobtitle, :keywords, :library, :netid, :phone, :space,
-                        :status, :subtitle, :title, :twitter, :publications, :blog, :about
+                        :status, :subtitle, :title, :twitter, :publications, :blog, :about,
                         :liaison_relationship
 
           def initialize(json_data)
