5ace5544c8ae6493c69f4c96917c7b0ec427e321
Eric Griffis <eric.griffis@nyu.edu>
2016-09-30 12:56:24 -0400

    Add cucumber helper tests

---
+++ spec/contented/cucumber_helpers/project_dir_helpers_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'cucumber_helpers/project_dir_helpers'
+
+RSpec.configure do |c|
+  c.include CucumberHelpers::ProjectDirHelpers, :include_helpers
+end
+
+describe CucumberHelpers::ProjectDirHelpers, :include_helpers do
+  describe "project_dir" do
+    subject{ project_dir }
+    it { is_expected.to eq File.expand_path(File.join(__FILE__, "..", "..", "..", "..")) }
+  end
+end
