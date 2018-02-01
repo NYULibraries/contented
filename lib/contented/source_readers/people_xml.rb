e1c5bd86e9ee773a52c21b1fd6c0c102d5b6132b
CircleCI-NYULib <lib-webservices@nyu.edu>
2018-02-01 14:53:45 -0500

    Preserve whitespace when loading People XML for markdowns sake in about section

--- lib/contented/source_readers/people_xml.rb
+++ lib/contented/source_readers/people_xml.rb
@@ -1,6 +1,6 @@
 require 'ox'
 require 'ostruct'
-#
+# Read XML file into an enumerable hash
 module Contented
   module SourceReaders
     class PeopleXML
@@ -14,7 +14,9 @@ module Contented
         end
         @file = file
         xml_string = File.read(file)
-        @people = Ox.load(xml_string, mode: :hash_no_attrs)[:DATA][:"staff-record"]
+        # hash_no_attrs => returns a hash and omits attributes from XML
+        # skip_none => ensures that the whitespace is preserved, which is important for Markdown
+        @people = Ox.load(xml_string, mode: :hash_no_attrs, skip: :skip_none)[:DATA][:"staff-record"]
       end
 
       def each(&block)
